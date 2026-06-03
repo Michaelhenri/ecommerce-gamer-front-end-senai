de7e883b9add4ec74d1043bc25ea26447e7b2db9
Michael Henrique <michaeldt2015@gmail.com>
2026-06-02 21:38:32 -0300

    feat: adicionado pagina de login e cadastro com transição entre as duas opções

--- js/jquery-script.js
+++ js/jquery-script.js
@@ -1,1 +1,24 @@
 //efeito de esconder formulário de cadastro
+
+$(document).ready(function () {
+
+    //Clicar em "Não é cadastrado ainda? Cadastre-se"
+    $("#link-ir-cadastro").click(function (e) {
+        e.preventDefault();
+
+        $("#section-login").fadeOut(400, function () {
+
+            $("#section-cadastro").removeClass("escondido").fadeIn(400);
+        });
+    });
+
+    //Clicar em "Já tem uma conta? Faça Login"
+    $("#link-ir-login").click(function (e) {
+        e.preventDefault();
+
+        $("#section-cadastro").fadeOut(400, function () {
+
+            $("#section-login").fadeIn(400);
+        });
+    });
+});
